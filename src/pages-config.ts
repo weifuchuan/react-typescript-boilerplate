79e82818e6010f08d6264e3ac985259309afea70
wei fuchuan <fuchuan.wei@hotmail.com>
2018-11-30 10:02:07 +0800

    Update pages-config.ts

--- src/pages-config.ts
+++ src/pages-config.ts
@@ -1,5 +1,6 @@
 export interface Page {
 	name: string; // 页面名，需与目录名相同
+	entry?: string; // 入口js|jsx|ts|tsx文件，默认index.tsx
 	template?: string; // .html模板文件名，如有，应为相对于该页面目录的html文件路径；否则为 public/index.html
 	filename?: string; // 输出文件名，默认 <name>.html
 	title?: string; // html的title, 默认 name
@@ -12,8 +13,13 @@ export default [
 	{
 		name: 'todo-app-with-redux',
 		template: 'index.html'
-	}, 
+	},
 	{
-		name:"with-redux-saga"
+		name: 'with-redux-saga'
+	},
+	{
+		name: 'breakout',
+		template: 'index.html',
+		entry:"index.ts"
 	}
 ] as Page[];
