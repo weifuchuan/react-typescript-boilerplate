3d3622b00a86ae49b19c2dbc9edd12fde084326f
wei fuchuan <fuchuan.wei@hotmail.com>
2019-02-25 22:02:54 +0800

    Update polyfills.js

--- config/polyfills.js
+++ config/polyfills.js
@@ -1,6 +1,12 @@
 'use strict';
 
 // use ` require("xxx") ` for import polyfils
+
 require("react-app-polyfill/ie9")
 require("intersection-observer/intersection-observer.js")
-require("resize-observer-polyfill/dist/ResizeObserver.js")
+
+import _ResizeObserver from 'resize-observer-polyfill';
+
+if (!window.ResizeObserver) {
+  window.ResizeObserver = _ResizeObserver;
+}
