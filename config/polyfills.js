732c0a3688a2e011965f323eb22079a13c6b7ffb
wei fuchuan <fuchuan.wei@hotmail.com>
2019-02-25 21:44:01 +0800

    fix polyfill bug

--- config/polyfills.js
+++ config/polyfills.js
@@ -1,6 +1,6 @@
 'use strict';
 
 // use ` require("xxx") ` for import polyfils
-require("'react-app-polyfill/ie9'")
+require("react-app-polyfill/ie9")
 require("intersection-observer/intersection-observer.js")
 require("resize-observer-polyfill/dist/ResizeObserver.js")
