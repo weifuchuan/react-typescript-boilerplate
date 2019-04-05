67e8cd879a0c7b31c641c47fc481a29a661223a8
wei fuchuan <fuchuan.wei@hotmail.com>
2019-04-05 10:09:23 +0800

    Update polyfills.js

--- config/polyfills.js
+++ config/polyfills.js
@@ -8,5 +8,4 @@ require("intersection-observer/intersection-observer.js")
 
 import EventEmitter from "wolfy87-eventemitter";
 
-window.EventEmitter = EventEmitter;
-window.bus = new EventEmitter();
+window.EventEmitter = EventEmitter; 
